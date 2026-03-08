ea0ee8e18a9ebdab1cf287f7cbf900ec08bdcf0c
panelrock24 <emplealinknacional@gmail.com>
2026-03-08 00:44:18 -0500

    Listo para el despliegue3

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'article/:slug',
+    renderMode: RenderMode.Client // <--- Esto le dice a Angular: "No intentes pre-renderizar esto"
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
